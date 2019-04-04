17d15c4db2f0f6ded1915271863a1e4808d20d5a
Eligrive <47270510+Eligrive@users.noreply.github.com>
2019-04-04 19:54:40 +0200

    Update API_Request.js

--- src/API/KDR_API/API_Request.js
+++ src/API/KDR_API/API_Request.js
@@ -1,4 +1,2 @@
-var KdRequest = new Request("https://api.fortnitetracker.com/v1/profile/pc/Eligrive", "d3d1a67c-8e55-42b9-90fe-ae32bb72b800")
-{
-    var playerName = KdRequest.epicUserHandle   
-}
+
+fetch('https://api.fortnitetracker.com/v1/profile/pc/Eligrive/' , myInit).then
